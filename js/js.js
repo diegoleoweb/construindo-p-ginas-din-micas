6157d0b1c5f4df2e681da2785276d2cd3e300904
unknown <diegoleweb@gmail.com>
2025-01-15 20:18:55 -0300

    Adicionando addEventListener

--- js/js.js
+++ js/js.js
@@ -1,2 +1,9 @@
 /*utilizando o DOM para selecionar i input*/
 const inputItem = document.getElementById("input-item");
+
+const botaoAdicionar =  document.getElementById("adicionar-item");
+
+botaoAdicionar.addEventListener("click", (evento) =>{ //espera um evento 
+    evento.preventDefault();//evitar o comportamento padrão do formulário.
+    console.log(inputItem.value);
+})
